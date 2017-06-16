b51a00fdb7c75caf7fe4073d25d6d6505705b5d9
Alex <alex@Alexs-Mac-mini.local>
2017-06-16 11:19:09 -0700

    refinerycms test

---
+++ rails/cms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = 'f39f96665319fd92e0eaf3fc31460b066a7e42d1e2cc3df0c202e779d6c89b4f67146b6bc83753421a4f0d5568416f4650b6f599f113a7e1a9e0ea923b02a1bb'
