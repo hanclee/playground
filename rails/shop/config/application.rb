1aee1d722c28731abc2b578f23b77cb711f0879b
Alex <alex@Alexs-Mac-mini.local>
2017-06-16 16:44:00 -0700

    spree store

--- rails/shop/config/application.rb
+++ rails/shop/config/application.rb
@@ -8,8 +8,18 @@ Bundler.require(*Rails.groups)
 
 module Shop
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
