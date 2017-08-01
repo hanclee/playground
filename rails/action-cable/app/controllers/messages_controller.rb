9bb2237cc7f006734c49754a4b17d00bf1298e61
Han Lee <hanclee@yahoo.com>
2017-08-01 13:02:29 -0700

    get websocket updates working

--- rails/action-cable/app/controllers/messages_controller.rb
+++ rails/action-cable/app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
-    message.user = current_user
+    # message.user = current_user
+    message.user = User.find(1)
     if message.save
-      # do some stuff
-    else
-      redirect_to chatrooms_path
+      ActionCable.server.broadcast 'messages', message: message.content, user: message.user.username
+      head :ok
     end
   end
 
